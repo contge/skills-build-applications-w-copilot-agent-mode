d6307ac4b4dbc83f46bdfb2b49e1cf74f59d8648
contge <glcontrerasc@gmail.com>
2025-04-08 02:01:36 +0000

    Checked settings.py for errors and confirmed functionality

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,29 @@
+from djongo import models
+
+class User(models.Model):
+    _id = models.ObjectIdField(primary_key=True)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+
+class Team(models.Model):
+    _id = models.ObjectIdField(primary_key=True)
+    name = models.CharField(max_length=255)
+    members = models.ManyToManyField(User)
+
+class Activity(models.Model):
+    _id = models.ObjectIdField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    activity_type = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    date = models.DateField()
+
+class Leaderboard(models.Model):
+    _id = models.ObjectIdField(primary_key=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    points = models.IntegerField()
+
+class Workout(models.Model):
+    _id = models.ObjectIdField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
